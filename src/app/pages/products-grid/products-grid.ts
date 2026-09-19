2125aa84c47f893d0e24f521c9d23521d0c0552b
Gabriel Godinho <gabrielgodi82@gmail.com>
2026-09-19 16:35:33 -0300

    Remoção do Titulo Games da Tela Inicial

--- src/app/pages/products-grid/products-grid.ts
+++ src/app/pages/products-grid/products-grid.ts
@@ -2,22 +2,11 @@ import { Component, inject, signal } from '@angular/core';
 
 import { ProductCard } from '../../components/product-card/product-card';
 
-import {
-  MatSidenavContainer,
-  MatSidenavContent,
-  MatSidenav
-} from '@angular/material/sidenav';
+import { MatSidenavContainer, MatSidenavContent, MatSidenav } from '@angular/material/sidenav';
 
-import {
-  MatNavList,
-  MatListItem,
-  MatListItemTitle
-} from '@angular/material/list';
+import { MatNavList, MatListItem, MatListItemTitle } from '@angular/material/list';
 
-import {
-  MatButton,
-  MatIconButton
-} from '@angular/material/button';
+import { MatButton, MatIconButton } from '@angular/material/button';
 
 import { MatIcon } from '@angular/material/icon';
 
@@ -50,98 +39,58 @@ import { ToggleWishlistButton } from '../../components/toggle-wishlist-button/to
 
   template: `
     <mat-sidenav-container class="h-full overflow-hidden">
-
       <mat-sidenav mode="side" opened="true">
-
         <div class="p-6">
-
-          <h2 class="menu-title">
-            Nosso Catálogo
-          </h2>
+          <h2 class="menu-title">Nosso Catálogo</h2>
 
           <mat-nav-list>
-
             @for (categoria of store.Categorias(); track categoria) {
-
               <a
                 mat-list-item
-
                 [activated]="
                   categoria === 'Todos os Jogos'
                     ? store.category() === 'Games'
                     : categoria === store.category()
                 "
-
                 class="my-2"
-
                 [routerLink]="
-                  categoria === 'Todos os Jogos'
-                    ? ['/products/Games']
-                    : ['/products', categoria]
+                  categoria === 'Todos os Jogos' ? ['/products/Games'] : ['/products', categoria]
                 "
               >
-
-                <span
-                  matListItemTitle
-                  class="font-medium"
-                >
+                <span matListItemTitle class="font-medium">
                   {{ categoria }}
                 </span>
-
               </a>
-
             }
-
           </mat-nav-list>
-
         </div>
-
       </mat-sidenav>
 
-
       <mat-sidenav-content class="bg-[#0f1117] p-6 overflow-y-auto">
-
         <div class="bg-[#0f1117] p-6">
-
-          <h1 class="text-2xl font-bold text-white mb-1">
-            {{ store.category() }}
-          </h1>
-
+          @if (store.category() !== 'Games') {
+            <h1 class="text-2xl font-bold text-white mb-1">
+              {{ store.category() }}
+            </h1>
+          }
         </div>
 
-
-        @if (
-          store.category() === 'Games' &&
-          store.carouselProducts().length > 0
-        ) {
-
+        @if (store.category() === 'Games' && store.carouselProducts().length > 0) {
           <div class="recommendations">
-
-            <h2 class="recommendations-title">
-              VEM VER NOSSAS RECOMENDAÇÕES!
-            </h2>
-
+            <h2 class="recommendations-title">VEM VER NOSSAS RECOMENDAÇÕES!</h2>
 
             <div class="carousel-wrapper">
-
               @if (store.carouselProducts().length > 1) {
-
                 <div class="side-card left-card">
-
                   <img
                     [src]="store.carouselProducts()[previousGameIndex].bannerUrl"
                     [alt]="store.carouselProducts()[previousGameIndex].name"
                   />
-
                 </div>
-
               }
 
-
               <section class="carousel">
-
                 <div class="carousel-image-wrapper">
-
                   <img
                     class="carousel-image"
                     [class.image-visible]="currentImageVisible()"
@@ -155,14 +104,10 @@ import { ToggleWishlistButton } from '../../components/toggle-wishlist-button/to
                     [src]="nextImage()"
                     [alt]="nextGameData?.name"
                   />
-
                 </div>
 
-
                 <div class="carousel-overlay">
-
                   <div class="carousel-info">
-
                     <h2>
                       {{ currentGameData?.name }}
                     </h2>
@@ -171,90 +116,57 @@ import { ToggleWishlistButton } from '../../components/toggle-wishlist-button/to
                       {{ currentGameData?.description }}
                     </p>
 
-
                     <div class="carousel-bottom">
-
                       <span class="carousel-price">
                         R$
                         {{ currentGameData?.price?.toFixed(2)?.replace('.', ',') }}
                       </span>
 
-                      <button class="carousel-buy-button">
-                        Comprar
-                      </button>
-
+                      <button class="carousel-buy-button">Comprar</button>
                     </div>
-
                   </div>
-
                 </div>
 
+                <button class="carousel-button prev" (click)="previousGame()">❮</button>
 
-                <button
-                  class="carousel-button prev"
-                  (click)="previousGame()"
-                >
-                  ❮
-                </button>
-
-                <button
-                  class="carousel-button next"
-                  (click)="nextGame()"
-                >
-                  ❯
-                </button>
-
+                <button class="carousel-button next" (click)="nextGame()">❯</button>
               </section>
 
-
               @if (store.carouselProducts().length > 1) {
-
                 <div class="side-card right-card">
-
                   <img
                     [src]="store.carouselProducts()[nextGameIndex].bannerUrl"
                     [alt]="store.carouselProducts()[nextGameIndex].name"
                   />
-
                 </div>
-
               }
-
             </div>
-
           </div>
-
         }
 
-
-        <p class="text-base text-gray-600 mb-6">
-          {{ store.filteredProducts().length }} Jogos
-        </p>
-
+        <p class="text-base text-gray-600 mb-6">{{ store.filteredProducts().length }} Jogos</p>
 
         <div class="responsive-grid">
-
-          @for (
-            product of store.filteredProducts();
-            track product.id
-          ) {
-
+          @for (product of store.filteredProducts(); track product.id) {
             <app-product-card [product]="product">
-              <app-toggle-wishlist-button !absolute z-10 top-3 right-3 w-10 h-10 rounded-full  [product]="product" />
+              <app-toggle-wishlist-button
+                !absolute
+                z-10
+                top-3
+                right-3
+                w-10
+                h-10
+                rounded-full
+                [product]="product"
+              />
             </app-product-card>
-
           }
-
         </div>
-
       </mat-sidenav-content>
-
     </mat-sidenav-container>
   `,
 })
-
 export default class ProductsGrid {
-
   store = inject(EcommerceStore);
 
   route = inject(ActivatedRoute);
@@ -275,28 +187,21 @@ export default class ProductsGrid {
 
   changingImage = false;
 
-
   constructor() {
-
     this.route.paramMap.subscribe((params) => {
-
       const categoria = params.get('category');
 
       if (categoria) {
         this.store.setCategory(categoria);
       }
-
     });
 
     this.loadCarousel();
 
     this.startAutoPlay();
-
   }
 
-
   loadCarousel() {
-
     const games = this.store.carouselProducts();
 
     if (games.length === 0) {
@@ -313,8 +218,7 @@ export default class ProductsGrid {
 
     const firstGame = games[order[0]];
 
-    const secondGame =
-      games[order.length > 1 ? order[1] : order[0]];
+    const secondGame = games[order.length > 1 ? order[1] : order[0]];
 
     this.currentImage.set(firstGame.bannerUrl);
 
@@ -323,12 +227,9 @@ export default class ProductsGrid {
     this.currentImageVisible.set(true);
 
     this.nextImageVisible.set(false);
-
   }
 
-
   get currentGameData() {
-
     const games = this.store.carouselProducts();
 
     const order = this.carouselOrder();
@@ -338,12 +239,9 @@ export default class ProductsGrid {
     }
 
     return games[order[this.carouselIndex()]];
-
   }
 
-
   get nextGameData() {
-
     const games = this.store.carouselProducts();
 
     const order = this.carouselOrder();
@@ -352,18 +250,12 @@ export default class ProductsGrid {
       return undefined;
     }
 
-    const nextPosition =
-      this.carouselIndex() === order.length - 1
-        ? 0
-        : this.carouselIndex() + 1;
+    const nextPosition = this.carouselIndex() === order.length - 1 ? 0 : this.carouselIndex() + 1;
 
     return games[order[nextPosition]];
-
   }
 
-
   get previousGameIndex(): number {
-
     const games = this.store.carouselProducts();
 
     const order = this.carouselOrder();
@@ -373,17 +265,12 @@ export default class ProductsGrid {
     }
 
     const previousPosition =
-      this.carouselIndex() === 0
-        ? order.length - 1
-        : this.carouselIndex() - 1;
+      this.carouselIndex() === 0 ? order.length - 1 : this.carouselIndex() - 1;
 
     return order[previousPosition];
-
   }
 
-
   get nextGameIndex(): number {
-
     const games = this.store.carouselProducts();
 
     const order = this.carouselOrder();
@@ -392,82 +279,49 @@ export default class ProductsGrid {
       return 0;
     }
 
-    const nextPosition =
-      this.carouselIndex() === order.length - 1
-        ? 0
-        : this.carouselIndex() + 1;
+    const nextPosition = this.carouselIndex() === order.length - 1 ? 0 : this.carouselIndex() + 1;
 
     return order[nextPosition];
-
   }
 
-
   startAutoPlay() {
-
     clearTimeout(this.autoPlayTimer);
 
     this.autoPlayTimer = setTimeout(() => {
-
       this.nextGame();
-
     }, 10000);
-
   }
-
 
   previousGame() {
-
     const order = this.carouselOrder();
 
-    if (
-      order.length === 0 ||
-      this.changingImage
-    ) {
+    if (order.length === 0 || this.changingImage) {
       return;
     }
 
-    const newIndex =
-      this.carouselIndex() === 0
-        ? order.length - 1
-        : this.carouselIndex() - 1;
+    const newIndex = this.carouselIndex() === 0 ? order.length - 1 : this.carouselIndex() - 1;
 
     this.changeImage(newIndex);
-
   }
-
 
   nextGame() {
-
     const order = this.carouselOrder();
 
-    if (
-      order.length === 0 ||
-      this.changingImage
-    ) {
+    if (order.length === 0 || this.changingImage) {
       return;
     }
 
-    const newIndex =
-      this.carouselIndex() === order.length - 1
-        ? 0
-        : this.carouselIndex() + 1;
+    const newIndex = this.carouselIndex() === order.length - 1 ? 0 : this.carouselIndex() + 1;
 
     this.changeImage(newIndex);
-
   }
 
-
   changeImage(index: number) {
-
     const games = this.store.carouselProducts();
 
     const order = this.carouselOrder();
 
-    if (
-      games.length === 0 ||
-      order.length === 0 ||
-      this.changingImage
-    ) {
+    if (games.length === 0 || order.length === 0 || this.changingImage) {
       return;
     }
 
@@ -475,26 +329,18 @@ export default class ProductsGrid {
 
     const newRealIndex = order[index];
 
-    this.nextImage.set(
-      games[newRealIndex].bannerUrl
-    );
+    this.nextImage.set(games[newRealIndex].bannerUrl);
 
     this.carouselIndex.set(index);
 
     requestAnimationFrame(() => {
-
       this.nextImageVisible.set(true);
 
       this.currentImageVisible.set(false);
-
     });
 
-
     setTimeout(() => {
-
-      this.currentImage.set(
-        games[newRealIndex].bannerUrl
-      );
+      this.currentImage.set(games[newRealIndex].bannerUrl);
 
       this.currentImageVisible.set(true);
 
@@ -503,16 +349,10 @@ export default class ProductsGrid {
       this.changingImage = false;
 
       this.startAutoPlay();
-
     }, 400);
-
   }
-
 
   goToGame(index: number) {
-
     this.changeImage(index);
-
   }
-
 }
