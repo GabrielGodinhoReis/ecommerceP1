25594eb1659d6f987db293ac155cadb1bea7616c
Gabriel Godinho <gabrielgodi82@gmail.com>
2026-09-19 16:17:46 -0300

    teste

--- src/app/pages/my-wishlist/empty-wishlist/empty-wishlist.ts
+++ src/app/pages/my-wishlist/empty-wishlist/empty-wishlist.ts
@@ -28,7 +28,7 @@ import { RouterLink } from '@angular/router';
         routerLink="/products/Games"
         class="-ms-2 flex items-center gap-2 rounded-xl !bg-gray-700 !px-4 !py-2 !text-sm !font-medium !text-gray-100 shadow-sm transition-all duration-200 hover:!bg-[#8b5cf6] hover:!shadow-md active:scale-95"
       >
-        Começar a comprar
+        Confira Nossas Ofertas
       </button>
 
     </div>
